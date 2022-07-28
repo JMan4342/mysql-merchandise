aae80ea32d2cab11df7bb5e5efcc82ba4fb7ba91
Joseph Manning <jgmanning3@yahoo.com>
2022-07-27 18:32:04 -0700

    Create get route to fetch individual item info

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const { addItem, getItem } = require("./utils/queryHelpers");
 const app = express();
@@ -6,13 +7,13 @@ const genericError = "Sorry, something went wrong!";
 
 app.use(express.json());
 
-app.post("/", async function (req, res) {
+app.post("/post", async function (req, res) {
   try {
     const { name, category, description } = req.body;
     const [result] = await addItem(name, category, description);
-    if (result.insertId) {
-      const [data] = await getItem(result.insertId);
-      res.send({ success: true, result: data[0] });
+    if (result.insertedId) {
+      const [data] = await getItem(result.insertedId);
+      res.send({ success: true, result: data });
     } else {
       res.status(500).send({
         success: false,
@@ -27,5 +28,26 @@ app.post("/", async function (req, res) {
   }
 });
 
-app.listen(8000);
-console.log("Listening on localhost 8000");
+app.get("/:id", async function(req, res) {
+    const {id} = req.params;
+    try {
+        const [result] = await getItem(id);
+        if (result.length > 0) {
+            res.send({success: true, result: result[0]})
+        } else {
+            res.status(404).send({
+                success: false,
+                error: `No item found with id ${id}`,
+            })
+        }
+    } catch (error) {
+        res.status(500).send({
+            success: false,
+            error: genericError,
+        })
+    }
+})
+
+app.listen(8000, () => {
+  console.log("Server is listening on port 8000");
+});
--- utils/queryHelpers.js
+++ utils/queryHelpers.js
@@ -8,6 +8,15 @@ const addItem = async (name, category, description) => {
   );
 };
 
+const getItem = async (id) => {
+  const connection = await getConnection();
+  return connection.execute(
+    "select i.id,i.name,c.name category,i.description from item i, category c where i.category=c.id and i.id=?",
+    [id]
+  );
+};
+
 module.exports = {
   addItem,
+  getItem,
 };
