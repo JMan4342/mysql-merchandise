bc92522ca61582e16693ae1ebd2370586be8d30d
Joseph Manning <jgmanning3@yahoo.com>
2022-07-28 18:47:50 -0700

    Create routes to update item entry and delete item entry

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const { response } = require("express");
 const express = require("express");
-const { addItem, getItem } = require("./utils/queryHelpers");
+const {
+  addItem,
+  getItem,
+  getAllItems,
+  updateDescription,
+  deleteItem,
+} = require("./utils/queryHelpers");
 const app = express();
 
 const genericError = "Sorry, something went wrong!";
 
 app.use(express.json());
 
-app.post("/post", async function (req, res) {
+app.get("/", async function (req, res) {
+  try {
+    const [result] = await getAllItems();
+    res.send({ success: true, result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error: genericError,
+    });
+  }
+});
+
+app.post("/", async function (req, res) {
   try {
     const { name, category, description } = req.body;
     const [result] = await addItem(name, category, description);
@@ -21,6 +39,7 @@ app.post("/post", async function (req, res) {
       });
     }
   } catch (error) {
+    console.log(error);
     res.status(500).send({
       success: false,
       error: genericError,
@@ -28,25 +47,70 @@ app.post("/post", async function (req, res) {
   }
 });
 
-app.get("/:id", async function(req, res) {
-    const {id} = req.params;
-    try {
-        const [result] = await getItem(id);
-        if (result.length > 0) {
-            res.send({success: true, result: result[0]})
-        } else {
-            res.status(404).send({
-                success: false,
-                error: `No item found with id ${id}`,
-            })
-        }
-    } catch (error) {
-        res.status(500).send({
-            success: false,
-            error: genericError,
-        })
+app.get("/:id", async function (req, res) {
+  const { id } = req.params;
+  try {
+    const [result] = await getItem(id);
+    if (result.length > 0) {
+      res.send({ success: true, result: result[0] });
+    } else {
+      res.status(404).send({
+        success: false,
+        error: `No item found with id ${id}`,
+      });
     }
-})
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error: genericError,
+    });
+  }
+});
+
+app.put("/:id", async function (req, res) {
+  try {
+    const { name, category, description } = req.body;
+    const { id } = req.params;
+    const [result] = await updateDescription(id, name, category, description);
+    if (result.affectedRows > 0) {
+      const [data] = await getItem(id);
+      res.send({ success: true, result: data[0] });
+    } else {
+      res.status(400).send({
+        success: false,
+        error: genericError,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error: genericError,
+    });
+  }
+});
+
+app.delete("/:id", async function (req, res) {
+  try {
+    const { id } = req.params;
+    const [result] = await deleteItem(id);
+    if (result.affectedRows > 0) {
+      res.send({ success: true, message: "Item successfully deleted" });
+    } else {
+      res.status(400).send({
+        success: false,
+        error: genericError,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error: genericError,
+    });
+  }
+});
 
 app.listen(8000, () => {
   console.log("Server is listening on port 8000");
--- utils/queryHelpers.js
+++ utils/queryHelpers.js
@@ -3,8 +3,15 @@ const getConnection = require("./db");
 const addItem = async (name, category, description) => {
   const connection = await getConnection();
   return connection.execute(
-    "INSERT INTO ITEM (name, category, description) values (?,?,?)",
+    "INSERT INTO Item (name, category, description) VALUES (?,?,?)",
     [name, category, description]
+  );
+};
+
+const getAllItems = async () => {
+  const connection = await getConnection();
+  return connection.execute(
+    "select i.id,i.name,c.name category,i.description from item i,category c where i.category=c.id order by i.id"
   );
 };
 
@@ -16,7 +23,23 @@ const getItem = async (id) => {
   );
 };
 
+const updateDescription = async (id, name, category, description) => {
+  const connection = await getConnection();
+  return connection.execute(
+    "UPDATE item SET name=?, category=?, description=? WHERE id=?",
+    [name, category, description, id]
+  );
+};
+
+const deleteItem = async (id) => {
+  const connection = await getConnection();
+  return connection.execute("DELETE from item where id=?", [id]);
+};
+
 module.exports = {
   addItem,
+  getAllItems,
   getItem,
+  updateDescription,
+  deleteItem,
 };
